c4143588bafc4988e03e7a295faa4a8335b97ff0
Alex Reff <alex-reff@am.com>
2019-06-19 16:31:16 -0500

    Added submodule + fixed webpack 'NODE_ENV' logic

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,57 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const devMode = process.env.NODE_ENV !== 'production';
 
-module.exports = {
-    entry: './src/index.ts',
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js']
-    },
-    devtool: "source-map",
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.s(a|c)ss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    {
-                        loader: 'css-loader',
-                        options:
+module.exports = (env, argv) => {
+    const isDev = argv.mode !== 'production';
+
+    return {
+        entry: './src/index.ts',
+        resolve: {
+            extensions: ['.ts', '.tsx', '.js']
+        },
+        devtool: isDev ? "source-map" : false,
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, 'dist')
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.s(a|c)ss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
                         {
-                            url: false,
-                            sourceMap: true
-                        }
-                    },
-                    {
-                        loader: 'sass-loader',
-                        options:
+                            loader: 'css-loader',
+                            options:
+                            {
+                                url: false,
+                                sourceMap: true
+                            }
+                        },
                         {
-                            sourceMap: true
+                            loader: 'sass-loader',
+                            options:
+                            {
+                                sourceMap: true
+                            }
                         }
-                    }
-                ]
-            },
-            { 
-                test: /\.tsx?$/, 
-                loader: 'awesome-typescript-loader'
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new MiniCssExtractPlugin({
-            filename: devMode ? 'app.css' : 'app.[hash].css',
-        })
-    ],
-    mode : devMode ? 'development' : 'production'
+                    ]
+                },
+                { 
+                    test: /\.tsx?$/, 
+                    loader: 'awesome-typescript-loader'
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html'
+            }),
+            new MiniCssExtractPlugin({
+                filename: 'app.css'
+            })
+        ],
+        mode: isDev ? 'development' : 'production'
+    };
 };
